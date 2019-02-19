5a76d20fbc069c95231244b8377b1715c080d933
Jay <jay.verhoef@gmail.com>
2019-02-19 07:22:16 -0900

    updating script on moose data

--- inst/scripts/moose_data_example_code.r
+++ inst/scripts/moose_data_example_code.r
@@ -3,36 +3,99 @@ library(sp)
 library(rgeos)
 library(rgdal)
 
+# use Alaska Moose data
 data(AKmoose)
+# plot data, which are in polygons in Alaska Albers projection
+plot(AKmoose)
+AKmoose@proj4string
+# see http://spatialreference.org/ref/epsg/nad83-alaska-albers/
 
+# transform the projection to lat/lon
 latlon = spTransform(AKmoose, CRS("+init=epsg:4326"))
 plot(latlon)
+latlon@proj4string
+# see http://spatialreference.org/ref/epsg/4326/
 
-#get centroids
+#get centroids of polygons
 centroids = data.frame(ID = latlon@data$ID, 
   x = gCentroid(latlon,byid=TRUE)@coords[,'x'],
   y = gCentroid(latlon,byid=TRUE)@coords[,'y'])
 
+# create user-defined UTM coordinates for spatial modeling
 xy = LLtoUTM(mean(centroids$x), centroids$y, centroids$x)$xy
 
+# pull the data.frame from the SpatialPolygonsDataFrame object
 d1 = AKmoose@data
 str(d1)
+# add new UTM coordinates to data.frame, 
 d1$x = xy[,'x']
 d1$y = xy[,'y']
+# check to make sure that ordering didn't get messed up 
+# by comparing to lat lon in the data frame
+str(d1)
+d1[1:30, c('CENTRLAT', 'y', 'CENTRLON','x')]
 
-#undebug(slmfit)
-slmfit_out = slmfit(TOTAL ~ STRAT, data = d1, xcoordcol = 'x', ycoordcol = 'y',
+# fit a model where STRAT is a covariate
+slmfit_out1 = slmfit(TOTAL ~ STRAT, data = d1, xcoordcol = 'x', ycoordcol = 'y',
   CorModel = "Exponential",
   coordtype = "UTM")
+# summary of fitted model
+summary(slmfit_out1)
+# function to get R^2
+GR2(slmfit_out1)
 
-predout1 = FPBKpred(slmfit_out)
+# predictions
+predout1 = FPBKpred(slmfit_out1)
 
 #default map
 plot(predout1)
 
-#change a few parameters
+#change a few parameters for plotting predictions
 plot(predout1, nbreaks = 20, breakMethod = 'even', cex = 2,
-  legend.cex = 1.4)
+  legend.cex = 1.3)
+
+# The total estimate of moose,
+predout1$FPBK_Prediction
+# and its standard error
+sqrt(predout1$PredVar)
+# yielding a coefficient of variation (CV)
+sqrt(predout1$PredVar)/predout1$FPBK_Prediction
+
+# Try a model without stratification
+slmfit_out2 = slmfit(TOTAL ~ 1, data = d1, xcoordcol = 'x', ycoordcol = 'y',
+  CorModel = "Exponential",
+  coordtype = "UTM")
+
+# predictions
+predout2 = FPBKpred(slmfit_out2)
+# The total estimate of moose,
+predout2$FPBK_Prediction
+# and its standard error
+sqrt(predout2$PredVar)
+# yielding a coefficient of variation (CV)
+sqrt(predout2$PredVar)/predout2$FPBK_Prediction
+
+# Do estimates by strata
+slmfit_outL = slmfit(TOTAL ~ 1, data = d1[d1$STRAT == 'L',], xcoordcol = 'x', ycoordcol = 'y',
+  CorModel = "Exponential",
+  coordtype = "UTM")
+slmfit_outM = slmfit(TOTAL ~ 1, data = d1[d1$STRAT == 'M',], xcoordcol = 'x', ycoordcol = 'y',
+  CorModel = "Exponential",
+  coordtype = "UTM")
+
+# prediction
+predoutL = FPBKpred(slmfit_outL)
+predoutM = FPBKpred(slmfit_outM)
+# The total estimate of moose,
+predoutL$FPBK_Prediction + predoutM$FPBK_Prediction
+# and its standard error
+sqrt(predoutL$PredVar +predoutM$PredVar)
+# yielding a coefficient of variation (CV)
+sqrt(predoutL$PredVar +predoutM$PredVar)/
+  (predoutL$FPBK_Prediction + predoutM$FPBK_Prediction)
+
+
+
 
 library(splmm)
 d2 = d1
