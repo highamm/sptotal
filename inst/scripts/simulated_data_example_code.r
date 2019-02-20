717fbe82b2aac17f699511fb4cfb5df7599ee43e
Jay <jay.verhoef@gmail.com>
2019-02-19 16:46:13 -0900

    adding script on simulated data

--- inst/scripts/simulated_data_example_code.r
+++ inst/scripts/simulated_data_example_code.r
@@ -1,51 +1,6 @@
 library(sptotal)
 
-#create systematic grid of points
-xy = pointSimSyst(nrow = 30, ncol = 30, lower_x_lim = 0, upper_x_lim = 1, 
-    lower_y_lim = 0, upper_y_lim = 1) 
-    
-plot(xy)
 
-set.seed(1)
-
-# create autocorrelated random error structure
-eps = geostatSim(xy, xcol = "x", ycol = "y", 
-	parsil = 1, range = 1, nugget = .01,
-	minorp = 1, rotate = 90, extrap = NULL,
-	CorModel = "Exponential")[,3]
-
-#create independent continuous X1 variable
-X1 = rnorm(nrow(xyz))
-#create independent continuous X2 variable
-X2 = rnorm(nrow(xyz))
-#create independent continuous X3 variable
-X3 = rnorm(nrow(xyz))
-#create independent continuous X4 variable
-X4 = rnorm(nrow(xyz))
-#create independent continuous X5 variable
-X5 = rnorm(nrow(xyz))
-#create autocorrelated continuous X6 variable
-X6 = geostatSim(xy, xcol = "x", ycol = "y", 
-	parsil = 1, range = .2, nugget = .01,
-	minorp = 1, rotate = 90, extrap = NULL,
-	CorModel = "Exponential")[,3]
-#create autocorrelated continuous X7 variable
-X7 = geostatSim(xy, xcol = "x", ycol = "y", 
-	parsil = 1, range = .2, nugget = .01,
-	minorp = 1, rotate = 90, extrap = NULL,
-	CorModel = "Exponential")[,3]
-#create factor F1 variable
-F1 = as.factor(rep(1:3, times = nrow(xyz)/3))
-F1 = F1[order(runif(nrow(xyz)))]
-#create factor F2 variable
-F2 = as.factor(rep(1:5, times = nrow(xyz)/5))
-F2 = F2[order(runif(nrow(xyz)))]
-
-Z = 10 + 0*X1 + .1*X2 + .2*X3 + .3*X4 + .4*X5 + .4*X6 + .1*X7 + 
-  .4*(as.numeric(F1) - 2) + .1*(as.numeric(F2) - 3) + 2*eps
-  
-simdata = data.frame(x = xy$x, y = xy$y, X1 = X1, X2 = X2, X3 = X3, X4 = X4,
-  X5 = X5, X6 = X6, X7 = X7, F1 = F1, F2 = F2, Z = Z)
 obsID = sample(1:nrow(xyz), 100)
 simobs = simdata
 simobs[!(1:nrow(xyz) %in% obsID),'Z'] = NA
