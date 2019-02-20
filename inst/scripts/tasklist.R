b952948e95865c6cf02e6cdfc767bcc2dfc9b83b
Matt Higham <highamm@oregonstate.edu>
2019-02-20 10:31:35 -0800

    Task List

---
+++ inst/scripts/tasklist.R
@@ -0,0 +1,20 @@
+## Task List
+##
+## Create AIC.slmfit function
+## Imput covariance parameters
+## helper function to get coordinates from sp objects
+## helper function to change coordinates from latlon to UTM
+## Residual function and Empricial Variogram
+##
+## Vignette
+## Background and Introduction of Method (Matt)
+## Pre-analysis preparation
+## Fit Model
+## Diagnostics
+## Prediction and Plotting
+
+## Show analyses of our data sets
+
+## Show sptotal has approximate covergae and unbiasedness for
+## an example
+## Compare to yimpute, spsurvey, simple random sample
