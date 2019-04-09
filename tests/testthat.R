fde9420550058b5ddcd64389a5c67b7701632dd4
brycefrank <bryce.frank@oregonstate.edu>
2019-04-09 14:42:18 -0700

    adding three tests for m2LL.FPBK.nodet

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,20 @@
 library(testthat)
 library(sptotal)
 
-test_check("sptotal")
+test_that('m2LL.FPBK.nodet runs for exponential covariance model - REML.', {
+  sptotal::m2LL.FPBK.nodet(theta = c(1, 1, 1), zcol=simdata$Z, XDesign=as.matrix(simdata[,c(3:9)]),
+                           xcoord=simdata$x, ycoord=simdata$y, CorModel="Exponential",
+                           estmethod="REML")
+})
+
+test_that('m2LL.FPBK.nodet runs for exponential covariance model - ML.', {
+  sptotal::m2LL.FPBK.nodet(theta = c(1, 1, 1), zcol=simdata$Z, XDesign=as.matrix(simdata[,c(3:9)]),
+                           xcoord=simdata$x, ycoord=simdata$y, CorModel="Exponential",
+                           estmethod="ML")
+})
+
+test_that('m2LL.FPBK.nodet runs for spherical covariance model - REML.', {
+  sptotal::m2LL.FPBK.nodet(theta = c(1, 1, 1), zcol=simdata$Z, XDesign=as.matrix(simdata[,c(3:9)]),
+                           xcoord=simdata$x, ycoord=simdata$y, CorModel="Spherical",
+                           estmethod="REML")
+})
