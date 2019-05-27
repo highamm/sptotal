2c1d301778dbb7a20ecdbef8e90982bbcfd9f859
brycefrank <bryce.frank@oregonstate.edu>
2019-05-27 11:07:22 -0700

    adding branch

---
+++ tests/testthat/test_sptotal.R
@@ -0,0 +1,63 @@
+library(testthat)
+library(sptotal)
+library(assertthat)
+
+set.seed(12)
+
+## REML ##
+test_that('m2LL.FPBK.nodet runs for exponential covariance model - REML.', {
+  res <- sptotal::m2LL.FPBK.nodet(theta = c(1, 1, 1), zcol=simdata$Z, XDesign=as.matrix(simdata[,c(3:9)]),
+                           xcoord=simdata$x, ycoord=simdata$y, CorModel="Exponential",
+                           estmethod="REML")
+  res <- round(res, 3)
+  expect_equal(res[1,1], 1484.494)
+})
+
+test_that('m2LL.FPBK.nodet runs for spherical covariance model - REML.', {
+  res <- sptotal::m2LL.FPBK.nodet(theta = c(1, 1, 1), zcol=simdata$Z, XDesign=as.matrix(simdata[,c(3:9)]),
+                           xcoord=simdata$x, ycoord=simdata$y, CorModel="Spherical",
+                           estmethod="REML")
+  res <- round(res, 3)
+  expect_equal(res[1,1], 1519.875)
+})
+
+## ML ##
+
+test_that('m2LL.FPBK.nodet runs for exponential covariance model - ML.', {
+  res <- sptotal::m2LL.FPBK.nodet(theta = c(1, 1, 1), zcol=simdata$Z, XDesign=as.matrix(simdata[,c(3:9)]),
+                           xcoord=simdata$x, ycoord=simdata$y, CorModel="Exponential",
+                           estmethod="ML")
+  res <- round(res, 3)
+  expect_equal(res[1,1], 1462.294)
+})
+
+test_that('m2LL.FPBK.nodet runs for spherical covariance model - ML.', {
+  res <- sptotal::m2LL.FPBK.nodet(theta = c(1, 1, 1), zcol=simdata$Z, XDesign=as.matrix(simdata[,c(3:9)]),
+                           xcoord=simdata$x, ycoord=simdata$y, CorModel="Spherical",
+                           estmethod="ML")
+  res <- round(res, 3)
+  expect_equal(res[1,1], 1498.421)
+})
+
+test_that('Basic slmfit runs.', {
+  fit <- sptotal::slmfit(Z ~ X1 + X2 + X3 + X4+ X5 + X6 + X7, data=simdata, xcoordcol = "x", ycoordcol="y")
+
+  # TODO add more here, but this should be adequate enough to check for a break
+  known_coefs <- c(11.93583051, -0.08646128,  0.11470327,  0.18135598,  0.29403946,
+                   0.42163387,  0.45585942,  0.07337997)
+  expect_equal(fit$CoefficientEsts, known_coefs)
+})
+
+test_that('If the user provides None as the estmethod and an improper covestimates vetor, an error is raised.', {
+  expect_error(validate_covariance_inputs(estmethod = "None", covestimates = c(NA, NA, NA)))
+  expect_error(validate_covariance_inputs(estmethod = "None", covestimates = c(1, NA, NA)))
+})
+
+test_that('If column vectors are passed to validate_col_char, errors are raised.', {
+  expect_error(validate_col_char(simdata$x, simdata$y))
+})
+
+test_that('If coordinate vectors differ in length than the data, an error is raised.', {
+  expect_error(validate_data_length(simdata$x[,-10], simdata$y, simdata))
+  expect_error(validate_data_length(simdata$x, simdata$y[,-10], simdata))
+})
