7fc3d1f5b835c27c2f1c93aebf90c15fa5e0f63d
Matt Higham <highamm@oregonstate.edu>
2020-05-13 11:02:34 -0400

    Updated documentation of AIC and coef.

--- R/AIC.slmfit.R
+++ R/AIC.slmfit.R
@@ -1,14 +1,18 @@
-#' Extract AIC
+#' Extract the AIC from a \code{slmfit} object for comparing models.
 #'
-#' @param object a slmfit object
+#' @param object a \code{slmfit} object
 #' @param ... additional arguments
 #' @return The AIC value of the slmfit object.  Here, AIC is computed as 2 times the
 #' negative log-likelihood plus 2 times the number of model parameters.  Thus,
 #' lower AIC values are preferred models.
+#' @examples
+#' data(exampledataset) ## load a toy data set
+#' slmobj <- slmfit(formula = counts ~ pred1 + pred2, data = exampledataset,
+#' xcoordcol = 'xcoords', ycoordcol = 'ycoords', areacol = 'areavar')
+#' AIC(slmobj)
 #' @export
 
-AIC.slmfit <- function(object, ...)
-{
+AIC.slmfit <- function(object, ...) {
 
   if(object$FPBKpredobj$estmethod == 'ML')
     return(object$minus2loglike + 2 * length(object$SpatialParmEsts) +
--- R/coef.slmfit.R
+++ R/coef.slmfit.R
@@ -1,8 +1,13 @@
-#' Extract Model Coefficients
+#' Extract Model Coefficients from a \code{slmfit} object
 #'
-#' @param object a slmfit object
+#' @param object a \code{slmfit} object
 #' @param ... additional arguments
 #' @return a vector of fitted model coefficients.
+#' @examples
+#' data(exampledataset) ## load a toy data set
+#' slmobj <- slmfit(formula = counts ~ pred1 + pred2, data = exampledataset,
+#' xcoordcol = 'xcoords', ycoordcol = 'ycoords', areacol = 'areavar')
+#' coef(slmobj)
 #' @export
 
 coef.slmfit <- function(object, ...)
