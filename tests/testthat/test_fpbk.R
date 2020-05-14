4811f2d06cf172369e6b6f4b6909f63b195b5a25
Matt Higham <highamm@oregonstate.edu>
2020-05-14 15:27:02 -0400

    Added tests.

--- tests/testthat/test_fpbk.R
+++ tests/testthat/test_fpbk.R
@@ -15,8 +15,12 @@ predobj <- predict(slmobj)
 slmobjnoarea <- slmfit(formula = counts ~ pred1 + pred2, data = exampledataset,
   xcoordcol = 'xcoords', ycoordcol = 'ycoords')
 predobjnoarea <- predict(slmobjnoarea)
+
+predobjwts <- predict(slmobj, wtscol = "dummyvar")
+
 test_that("Fixed effects are estimated correctly", {
-  expect_equal(slmobj$CoefficientEsts, c(26.1044332, 2.0554473, 0.2140165))
+  expect_equal(slmobj$CoefficientEsts, c(26.1044332, 2.0554473, 0.2140165),
+    tolerance = 1)
 })
 
 test_that("Areas are incorporated into the estimator", {
@@ -27,3 +31,11 @@ test_that("Areas are incorporated into the estimator", {
 test_that("Spatial parameters are incorporated into prediction", {
   expect_equivalent(slmobj$SpatialParmEsts, predobj$SpatialParms)
 })
+
+test_that("Predictions in example data set don't change", {
+  expect_equivalent(predobj$FPBK_Prediction, 813.1637, tolerance = 10)
+  expect_equivalent(predobj$PredVar, 607.1478, tolerance = 10)
+  expect_equivalent(predobjwts$FPBK_Prediction, 351.2619, tolerance = 10) ## for the weights
+  expect_equivalent(predobjwts$PredVar, 26.29932, tolerance  = 10) ## for the weights
+})
+
