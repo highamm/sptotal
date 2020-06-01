b734ba811b5d76b979d39176be2614691411e2ee
jayverhoef <jay.verhoef@gmail.com>
2020-06-01 08:32:05 -0800

    Jays changes to vignette, help files for data sets

--- R/data.R
+++ R/data.R
@@ -19,45 +19,50 @@
 "exampledataset"
 
 
-#' Data Set with Alaska Moose Observations.
+#' Data Set with Alaska Moose Counts.
 #'
 #' A data set that can be used with the \code{sptotal} package. In
 #' this example, the counts are of moose on 860 sites of equal area.
 #'
-#' @format A spatial polygons (\code{sp}) object inclding:
+#' @format A SpatialPolygonsDataFrame object in the format of the (\code{sp}) package. The data frame \code{AKmoose@data} contains 860 rows and 4 columns:
 #' \describe{
 #'   \item{CENTRLAT}{The latitude of the centroid for each site}
 #'   \item{CENTRLON}{The latitude of the centroid for each site}
 #'   \item{STRAT}{A stratification variable}
 #'   \item{TOTAL}{The total moose count on each site}
-#'   ...
 #' }
+#' @source \href{http://www.adfg.alaska.gov/index.cfm?adfg=hunting.main}{Alaska Department of Fish and Game, Division of Wildlife Conservation} has released this data set under the CC0 (creative commons) license. To the extent possible under law, Alaska Department of Fish and Game, Division of Wildlife Conservation waives all copyright and related or neighboring rights to An Alaskan GSPE (Geospatial Population Estimator) Survey of Moose, AKmoose.rda. This work is published from: United States.
+#' @examples
+#' data(AKmoose) 
+#' names(AKmoose@data)
+#' summary(AKmoose@data)
 "AKmoose"
 
 #' Simulated Spatially Autocorrelated Data.
 #'
-#' A data set that can be used with the \code{sptotal} package. In
-#' this example, the response variable is a density.
+#' A simulated data set that can be used with the \code{sptotal} package.
 #'
 #' @format A data frame object inclding:
 #' \describe{
-#'   \item{x}{The x-coordinate of the centroid for each site}
-#'   \item{y}{The y-coordinate of the centroid for each site}
-#'   \item{X1}{Independent Variable to be used as a predictor}
-#'   \item{X2}{Independent Variable to be used as a predictor}
-#'   \item{X3}{Independent Variable to be used as a predictor}
-#'   \item{X4}{Independent Variable to be used as a predictor}
-#'   \item{X5}{Independent Variable to be used as a predictor}
-#'   \item{X6}{Spatially Correlated Random Variable}
-#'   \item{X7}{Spatially Correlated Random Variable}
-#'   \item{F1}{Factor Variable to be used as a predictor}
-#'   \item{F2}{Factor Variable to be used as a predictor}
-#'   \item{Z}{The total moose count on each site}
-#'   \item{wts1}{Possible Weighting Column to be used as the prediction weights}
-#'   \item{wts2}{Possible Weighting Column to be used as the prediction weights}
-
-#'   ...
+#'   \item{x}{The x-coordinate for each site}
+#'   \item{y}{The y-coordinate for each site}
+#'   \item{X1}{Simulated independent variable to be used as a predictor}
+#'   \item{X2}{Simulated independent variable to be used as a predictor}
+#'   \item{X3}{Simulated independent variable to be used as a predictor}
+#'   \item{X4}{Simulated independent variable to be used as a predictor}
+#'   \item{X5}{Simulated independent variable to be used as a predictor}
+#'   \item{X6}{Simulated spatially correlated random variable to be used as a predictor}
+#'   \item{X7}{Simulated spatially correlated random variable to be used as a predictor}
+#'   \item{F1}{Simulated factor variable to be used as a predictor}
+#'   \item{F2}{Simulated factor variable to be used as a predictor}
+#'   \item{Z}{The simulated response variable.}
+#'   \item{wts1}{Prediction weights if estimating an overall mean}
+#'   \item{wts2}{Prediction weights for estimating a total over a subset of 25 contiguous plots}
 #' }
+#' @examples
+#' data(simdata) 
+#' names(simdata)
+#' summary(simdata)
 "simdata"
 
 
@@ -69,7 +74,7 @@
 #' @format A data frame object inclding:
 #' \describe{
 #'   \item{coordinates}{The spatial cooredinates for the centroid for each site}
-#'   \item{BIOT_ha}{Biomass on Each Site}
+#'   \item{BIOT_ha}{Biomass on each site}
 #'   \item{precip_mean_30yr}{Average precipitation for 30 years}
 #'   \item{temp_mean_30yer}{Average temperature for 30 years}
 #'   ...
@@ -77,23 +82,25 @@
 "ORbiomass"
 
 
-#' U.S. Lakes Data
+#' Dissolved Organic Carbon in U.S. Lakes
 #'
-#' A data set that can be used with the \code{sptotal} package. In
-#' this example, the lakes are located across the United States.
+#' These data contain dissolved organic carbon (DOC) in National Lakes Data from the U.S. Environmental Protection Agency 
 #'
-#' @format A data frame object inclding:
+#' @format A data frame with 1206 rows and 9 variables:
 #' \describe{
-#'   \item{XCOORD}{The x spatial cooredinate for the centroid for each site}
-#'   \item{YCOORD}{The y spatial cooredinate for the centroid for each site}
-#'   \item{DOC_RESULT}{Disolved Organic Carbon}
-#'   \item{ELEVATION}{Variable that is a candidate for prediction}
-#'   \item{FCIBIG_LIT}{Variable that is a candidate for prediction}
-#'   \item{RVFCGNDBARE_RIP}{Variable that is a candidate for prediction}
-#'   \item{RVFCGNDWOODY_RIP}{Variable that is a candidate for prediction}
-#'   \item{RVFPUNDWOODY_RIP}{Variable that is a candidate for prediction}
-#'   \item{UID}{Lake Identification Number}
-
-#'   ...
+#'   \item{XCOORD}{x-coordinate from US Contiguous Albers Equal Area Conic projection}
+#'   \item{YCOORD}{y-coordinate from US Contiguous Albers Equal Area Conic projection}
+#'   \item{DOC_RESULT}{Analyte value, in mg/L, for Dissolved Organic Carbon}
+#'   \item{ELEVATION}{Elevation at lake coordinates (LAT_DD_N83, LON_DD_N83) from NHD Digital Elevation Map layer}
+#'   \item{FCIBIG_LIT}{Fish cover: index of fish cover due to large structures in the littoral zone}
+#'   \item{RVFCGNDBARE_RIP}{riparian zone and vegetation: fraction of ground lacking cover in the riparian zone}
+#'   \item{RVFCGNDWOODY_RIP}{riparian zone and vegetation: fraction of ground cover by woody vegetation in the riparian zone}
+#'   \item{RVFPUNDWOODY_RIP}{riparian zone and vegetation: fraction of understory with nonwoody cover present in the riparian zone}
+#'   \item{UID}{A unique lake identifier in the EPA lake survey datebases}
 #' }
+#' @source \href{National Aquatic Resource Surveys}{National Aquatic Resource Surveys} webpage.  We combined \href{https://www.epa.gov/sites/production/files/2016-12/nla2012_wide_siteinfo_08232016.csv}{site data}, \href{https://www.epa.gov/sites/production/files/2016-12/nla2012_waterchem_wide.csv}{DOC data}, and \href{https://www.epa.gov/sites/production/files/2016-12/nla2012_wide_phabmet_10202016.csv}{habitat metrics} to create a data set of 1206 lakes in the conterminous United States.
+#' @examples
+#' data(USlakes) 
+#' names(USlakes)
+#' summary(USlakes)
 "USlakes"
