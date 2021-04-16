1afdcfe512a9ba122e22164abb41e087a0665f53
Matt Higham <highamm@oregonstate.edu>
2021-04-16 15:19:10 -0400

    Added a print generic to the slmfit object.

--- R/print.summary.slmfit.R
+++ R/print.summary.slmfit.R
@@ -8,7 +8,6 @@
 #' @param signif.stars is an option to show which predictors
 #' are significant.
 #' @param ... further arguments passed to or from other methods.
-#'  \code{summary.slmfit}.
 #' @return a list with \itemize{
 #'   \item model formula
 #'   \item summary statistics for the residuals.
@@ -41,8 +40,8 @@ print.summary.slmfit <- function(x,
   print(resQ, digits = digits)
 
   cat("\nCoefficients:\n")
-  coefs = x$FixedEffects
-  colnames(coefs) = c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+  coefs <- x$FixedEffects
+  colnames(coefs) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
   printCoefmat(coefs, digits = digits, signif.stars = signif.stars,
     na.print = "NA", ...)
 
@@ -53,6 +52,39 @@ print.summary.slmfit <- function(x,
 
 }
 
-print.slmfit <- function(x,...) {
-  print(summary(x,...))
+#' Prints the fitted coefficient table of a fitted spatial linear model.
+#'
+#' This function uses the object that is output from \code{\link{slmfit}()} of class \code{slmfit}.
+#'
+#' @param x is an object generated from \code{\link{slmfit}()}
+#' @param digits is the number of digits to be displayed in the
+#' model output
+#' @param ... further arguments passed to or from other methods.
+#' @return a list with \itemize{
+#'   \item model formula
+#'   \item summary statistics for the residuals.
+#'   \item a table of fixed effects estimates and associated standard errors.
+#'   \item estimated spatial covariance parameter estimates.
+#'   \item generalized r-squared value.
+#' }
+#' @examples
+#' data(exampledataset) ## load a toy data set
+#' slmobj <- slmfit(formula = counts ~ pred1 + pred2, data = exampledataset,
+#' xcoordcol = 'xcoords', ycoordcol = 'ycoords', areacol = 'areavar')
+#' print(slmobj)
+#' @import stats
+#' @export
+
+print.slmfit <- function(x,
+                         digits = max(3L, getOption("digits") - 3L),
+                         ...) {
+
+  cat("\nCoefficients:\n")
+  coefs <- x$CoefficientEsts
+  names(coefs) <- x$PredictorNames
+
+  print(coefs, digits = digits)
+
+  cat("\nCovariance Parameters:\n")
+  print(x$SpatialParmEsts, digits = digits)
 }
