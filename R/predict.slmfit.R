3ed241bab32b2a96d2be37e02a3ac3338a7a893b
Matt Higham <highamm@oregonstate.edu>
2021-06-08 07:40:23 -0700

    not a bug after all.

--- R/predict.slmfit.R
+++ R/predict.slmfit.R
@@ -62,12 +62,11 @@ predict.slmfit <- function(object, wtscol = NULL,
   covparmests <- object$SpatialParmEsts
   areavar <- object$FPBKpredobj$areavar
 
-  ## if data set has weird names, it errors out
-  # if (is.null(wtscol) == FALSE) {
-  #   if (sum(names(data) == wtscol) == 0) {
-  #   stop("wtscol must be the name of the column (in quotes) in the data used in 'slmfit' that specifies the column with the prediction weights. ")
-  #   }
-  # }
+  if (is.null(wtscol) == FALSE) {
+    if (sum(names(data) == wtscol) == 0) {
+    stop("wtscol must be the name of the column (in quotes) in the data used in 'slmfit' that specifies the column with the prediction weights. ")
+    }
+  }
 
   # if (is.null(wtscol) == FALSE) {
   #   if (wtscol %in% names(data) == FALSE) {
